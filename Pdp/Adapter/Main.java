cd8f86ab4df9dee0e3541985f9c8e18e3d0e340f
Victor Emanuel Ramalho Ribeiro <verribeiro@hotmail.com>
2018-07-10 13:32:32 -0300

    Update Main.java

--- Pdp/Adapter/Main.java
+++ Pdp/Adapter/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    		int[][] vetor = new int[5][5];
+    		int[][] vetor = new int[5][2];
     		for(int i = 0; i < 5; i++) {
     			for(int j = 0; j < 5; j++) {
         			vetor[i][j] = j;
